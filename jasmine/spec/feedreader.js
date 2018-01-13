f45d70f7a2c3c3b722df0adcde21407b31803981
Jordi <ofineo@gmail.com>
2018-01-13 14:53:55 +0000

    Comments to the feedreader specifications

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,18 +1,12 @@
 /* feedreader.js
-*
-* This is the spec file that Jasmine will read and contains
-* all of the tests that will be run against your application.
-*/
+
 
 /* We're placing all of our tests within the $() function,
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
  */
 $(function () {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+    
     describe('RSS Feeds', function () {
 
         it('are defined', function () {
@@ -20,14 +14,17 @@ $(function () {
             expect(allFeeds.length).not.toBe(0);
         });
 
-        //on actual Jasmine i could have used .toBeNonEmptyString();
+        /*on the current version of Jasmine i could have used .toBeNonEmptyString();
+        *Test `allFeeds` object and ensures it has a URL defined and that the URL is not empty.
+        */
         it('URL defined', () => {
             allFeeds.forEach((feed) => {
                 expect(feed.url).toBeDefined();
                 expect(feed.url).toBeTruthy();
             });
         });
-
+        /*Test `allFeeds` object and ensures it has a name defined and that the name is not empty.
+        */
         it('name defined', () => {
             allFeeds.forEach((feed) => {
                 expect(feed.name).toBeDefined();
@@ -35,15 +32,19 @@ $(function () {
             });
         });
     });
-
+    
     describe('The menu', () => {
 
+        /*Test that the menu element is hidden by default.
+        */
         it('hidden by default', () => {
             let menuHidden = $('body').hasClass('menu-hidden');
 
             expect(menuHidden).toBe(true);
         });
 
+        /*Tests to make sure the menu changes visibility when the menu icon is clicked.
+        */
         it('shows on cick', () => {
             $('.menu-icon-link').trigger('click');
 
@@ -64,7 +65,9 @@ $(function () {
                 done();
             });
         });
-
+        /*Test `loadFeed` function. when called and complete, 
+        * it should be at least, a single `.entry` element within the `.feed` container.
+        */
         it('should have at least one entry', (done) => {
             let feedElement = $('.feed > a');
             expect(feedElement).not.toBeUndefined();
@@ -73,6 +76,7 @@ $(function () {
     });
 
     describe('New Feed Selection', () => {
+
         let firstFeed,
             secondFeed;
         beforeEach((done) => {
@@ -81,6 +85,9 @@ $(function () {
                 done();
             });
 
+            /*The second call to the function is wrapped in a conditional to make sure
+            * the first feeds are finished loading.
+            */
             if (feedLoadedFinished) {
                 loadFeed(1, () => {
                     secondFeed = $('.feed > a');
@@ -89,6 +96,9 @@ $(function () {
             }
         });
 
+        /*Test make sure that when a new feed is loaded by the `loadFeed` 
+        * function that the content actually changes.
+        */
         it('on load content changes', (done) => {
             if (feedLoadedFinished) {
                 expect(firstFeed).not.toBe(secondFeed);
@@ -99,12 +109,15 @@ $(function () {
 
     describe('Error handling', () => {
 
+        /*Check if variables are defined
+        */
         it('variables are defined', () => {
             expect(allFeeds).toBeDefined();
             expect(feedLoadedFinished).toBeDefined();
-
         });
 
+        /*Check if that when we call the Object is its bounds
+        */
         it('Array in bounds', () => {
             spyOn(window, 'loadFeed');
             loadFeed(0);
@@ -118,15 +131,20 @@ $(function () {
         beforeEach(() => {
             feedLenght = allFeeds.length;
             allFeeds.push( {
-                name: 'JW.org',
+                name: 'JW News',
                 url: 'https://www.jw.org/en/news/rss/FullNewsRSS/feed'
             });
         });
 
+        /*Test `allFeeds` object and ensures that the object increases when we add an extra feed.
+        */
         it('Add a feed', () => {        
             expect(allFeeds.length).toBe(feedLenght+1);
         });
 
+        /*Test `allFeeds` object and ensures that  the object decreases when we remove our 
+        *selected feed.
+        */
         it('Remove a feed', () => {
             allFeeds.splice(feedLenght,1);
             expect(allFeeds.length).toBe(feedLenght);
